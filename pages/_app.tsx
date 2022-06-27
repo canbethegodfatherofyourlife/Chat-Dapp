30b127197e4104c08d07cb28a1d6ba6515084525
Arka <arkadatta2002@gmail.com>
2022-06-27 10:08:02 +0530

    moralis server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }: AppProps) {
   return   (
   <MoralisProvider
-    appId={"process.env.NEXT_PUBLIC_APP_ID"} 
-    serverUrl={"process.env.NEXT_PUBLIC_SERVER_URL"}>
+    appId={`${process.env.NEXT_PUBLIC_APP_ID}`} 
+    serverUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}`}>
       <Component {...pageProps} />
   </MoralisProvider>
     )
